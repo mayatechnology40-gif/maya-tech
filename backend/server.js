0cdc11fce421b4ef539974f480ef480b1e655019
mayatechnology40-gif <mayatechnology40@gmail.com>
2026-05-09 11:47:20 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,10 @@ mongoose.connect(process.env.MONGODB_URI)
 
 // Email Transporter Setup
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  family: 4, // FORCE IPv4
   auth: {
     user: process.env.EMAIL_USER,
     pass: (process.env.EMAIL_PASS || '').replace(/\s+/g, '')
